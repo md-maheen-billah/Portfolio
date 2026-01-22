42332b19e961aab10eee37c90338e79d006355c3
Md Maheen Billah <md.maheen.billah@icloud.com>
2026-01-22 15:45:46 +0100

    updated image

--- src/app/page.js
+++ src/app/page.js
@@ -98,7 +98,7 @@ const HeaderBlock = () => (
     className="col-span-12 row-span-2 md:col-span-6"
   >
     <Image
-      src="https://i.ibb.co/JdCVyGK/463979909-3954349581449316-2195651006933051283-n.jpg"
+      src="https://i.ibb.co/9kMmxcdq/Chat-GPT-Image-Jan-22-2026-03-35-19-PM.png"
       alt="avatar"
       className="mb-4 border-4 border-maheenWT1 size-20 object-cover rounded-full"
       width={1200} // Specify width attribute
